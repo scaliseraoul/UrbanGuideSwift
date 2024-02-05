3eda3765da8c279fe74037ee1eb26702d25c670c
Raoul <scaliseraoul00@gmail.com>
2024-02-05 21:37:46 +0000

    Initial Commit

---
+++ UrbanGuide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UrbanGuide
+//
+//  Created by Raoul Scalise on 05/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UrbanGuide/UrbanGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanGuideApp.swift
+//  UrbanGuide
+//
+//  Created by Raoul Scalise on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct UrbanGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
